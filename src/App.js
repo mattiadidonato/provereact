59da12d5d32e742f3fca8aa145f72a8723393a20
mattia.didonato <mattia.didonato@testudosrl.com>
2021-02-06 12:27:22 +0100

    prove per imparare

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './component/component.js';
+import Users from './users.json';
+import React, { useState } from 'react';
+ 
+
+class Tabella extends React.Component {
+  state = {
+    data: new Date()
+  }
+  showdate = () => {
+    console.log(
+      this.state.data.toLocaleTimeString()
+    )
+  }
+  render(){
+    return(
+      <div>
+        <button onClick={this.showdate}>data</button>
+        
+        <div className="user_table">
+          <p>usando la state nelle classi</p>
+          <p>oggi è: {this.state.data.toLocaleTimeString()} </p>
+          {
+            Users.map(user => {
+              return (
+                <div key={user.id} className="tabrow">
+                  <p>{ user.name } { user.surname}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    )
+  }
+}
 
 function App() {
+  const [dataoggi, setdata] = useState(
+    {data: new Date()}
+  );
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <p>usando la useState() {dataoggi.data.toLocaleTimeString()}</p>
+      <Tabella/>
     </div>
   );
 }
 
 export default App;
+ 
+
+
---
+++ src/component/component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+
+class Header extends React.Component{
+    render(){
+        return <h3>header</h3>;
+    }
+}
+
+export default Header;
