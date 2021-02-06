548f07d7142738ff7b3dab9238ec8d9e1b918df0
mattia.didonato <mattia.didonato@testudosrl.com>
2021-02-06 19:45:47 +0100

    modifica

--- src/App.js
+++ src/App.js
@@ -36,6 +36,40 @@ class Tabella extends React.Component {
     )
   }
 }
+//componente Todo creato come funzione non come classe
+//gli passo un elemento della lista che scorro con la map()
+const Todo = (props) => {
+  return(
+    <div>      
+      {props.todo.name}
+      {props.todo.surname}
+    </div>
+  )
+}
+
+class Prova extends React.Component {
+  state = {
+    todos: [
+      {name: 'primo', completato: true, surname:'ds'},
+      {name: 'secondo', completato: false,surname:'ds'},
+      {name: 'terzo', completato: true,surname:'ds'}
+    ]
+  }
+  /*
+    nello state metto un array todos[] che poi scorro con la map()
+    leggendo l'elem fatto di name+completato e l'indice
+  */
+  render(){
+    return(
+      <div>
+        {this.state.todos.map((elem, index) => (
+          //ritorno un componente Todo per ogni elemento del todos[]
+          <Todo key={index} todo={elem} />
+        ))}
+      </div>
+    )
+  }
+}
 
 function App() {
   const [dataoggi, setdata] = useState({
@@ -46,7 +80,9 @@ function App() {
       <Header/>
       <p>usando la useState() {dataoggi.data.toLocaleTimeString()}</p>
       <Tabella/>
+      <Prova/>
     </div>
+    
   );
 }
 
